0fc7c0e4becafb3cc3a853a00abd91f67842c629
Felipe Marques <phellippemarques@gmail.com>
2021-02-21 01:13:49 -0300

    Atualizando

--- EXERCICIOS II - JS/D001/scripts/main.js
+++ EXERCICIOS II - JS/D001/scripts/main.js
@@ -2,27 +2,20 @@ function carregar(){
     let horas = new Date().getHours();
     let mensagem = document.getElementById('msg');
     let imagem = document.getElementById('img');
-    let txt = document.createTextNode(`Agora são ${horas} horas.`);
-    let img = document.createElement('img');
+    
+    mensagem.innerHTML = `Agora são <strong>${horas} horas!</horas>`;
+
     if (horas >= 0 && horas <= 4){
-        mensagem.appendChild(txt);
-        img.setAttribute('src', '');
-        imagem.appendChild(img);
+        imagem.src = './imagens/madrugada.png'
         document.body.style.background = '#1C1C1C';
     } else if (horas <= 12){
-        mensagem.appendChild(txt);
-        img.setAttribute('src', '');
-        imagem.appendChild(img);
+        imagem.src = './imagens/manha.png';
         document.body.style.background = '#FFD700';
     } else if (horas <= 17){
-        mensagem.appendChild(txt);
-        img.setAttribute('src', '');
-        imagem.appendChild(img);
+        imagem.src = './imagens/tarde.png';
         document.body.style.background = '#FF8C00';
     } else if (horas <= 23){
-        mensagem.appendChild(txt);
-        img.setAttribute('src', '');
-        imagem.appendChild(img);
+        imagem.src = './imagens/noite.png';
         document.body.style.background = '#696969';
     }
 }
